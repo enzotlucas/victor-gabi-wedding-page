5c6ae53ae3eda9b8fa7c7f0596d04c9d347af568
Your Name <enzothomazi@hotmail.com>
2026-05-07 23:21:57 -0300

    feat: Improve countdown to reflect singular numbers

--- scripts/countdown.js
+++ scripts/countdown.js
@@ -2,22 +2,46 @@
 const targetDate = new Date("2026-09-19T16:15:00");
 const oneSecond = 1000;
 
-const daysEl = document.getElementById("days");
-const hoursEl = document.getElementById("hours");
-const minutesEl = document.getElementById("minutes");
-const secondsEl = document.getElementById("seconds");
+const daysCounter = document.getElementById("days");
+const daysDescription = document.getElementById("days-description");
+const daysLabel = "DIA"
+
+const hoursCounter = document.getElementById("hours");
+const hoursDescription = document.getElementById("hours-description");
+const hoursLabel = "HORA"
+
+const minutesCounter = document.getElementById("minutes");
+const minutesDescription = document.getElementById("minutes-description");
+const minutesLabel = "MINUTO"
+
+const secondsCounter = document.getElementById("seconds");
+const secondsDescription = document.getElementById("seconds-description");
+const secondsLabel = "SEGUNDO"
+
+function startCountdown() {
+    if (!reachedTargetDate())
+        setInterval(updateCountdown, oneSecond);
+
+    updateCountdown();
+}
+
+function reachedTargetDate() {
+    const now = new Date();
+    const diff = targetDate - now;
+
+    return diff <= 0;
+}
 
 function updateCountdown() {
     const now = new Date();
     const diff = targetDate - now;
 
     if (diff <= 0) {
-        daysEl.textContent = 0;
-        hoursEl.textContent = 0;
-        minutesEl.textContent = 0;
-        secondsEl.textContent = 0;
+        updateContent(daysCounter, 0, daysDescription, daysLabel);
+        updateContent(hoursCounter, 0, hoursDescription, hoursLabel);
+        updateContent(minutesCounter, 0, minutesDescription, minutesLabel);
+        updateContent(secondsCounter, 0, secondsDescription, secondsLabel);
 
-        document.querySelector(".title").textContent = "O grande dia CHEGOU!";
         return;
     }
 
@@ -26,15 +50,14 @@ function updateCountdown() {
     const minutes = Math.floor((diff / (1000 * 60)) % 60);
     const seconds = Math.floor((diff / 1000) % 60);
 
-    daysEl.textContent = days;
-    hoursEl.textContent = hours;
-    minutesEl.textContent = minutes;
-    secondsEl.textContent = seconds;
+    updateContent(daysCounter, days, daysDescription, daysLabel);
+    updateContent(hoursCounter, hours, hoursDescription, hoursLabel);
+    updateContent(minutesCounter, minutes, minutesDescription, minutesLabel);
+    updateContent(secondsCounter, seconds, secondsDescription, secondsLabel);
 }
 
-function startCountdown() {
-    setInterval(updateCountdown, oneSecond);
-
-    updateCountdown();
+function updateContent(counter, count, description, label) {
+    counter.textContent = count;
+    description.textContent = count > 1 ? label + "S" : label;
 }
 
